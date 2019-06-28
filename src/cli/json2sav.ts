6ab837cbda3892fbaf0bd064d9d9e26539f7d596
bitowl <git@bitowl.de>
2019-06-28 15:23:11 +0200

    Add cli commands

---
+++ src/cli/json2sav.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+import * as fs from 'fs';
+import * as program from 'commander';
+
+import { Json2Sav } from '../json2sav';
+
+let sourceValue: string | undefined;
+let targetValue: string | undefined;
+
+function quitWithError(message: any) {
+  console.error(message);
+  process.exit(1);
+}
+
+program
+  .description('Converts from the more readable format (.json) ' +
+    'back to a Satisfactory save game (.sav)')
+  .arguments('<source> <target>')
+  .action((source, target) => {
+    sourceValue = source;
+    targetValue = target;
+  })
+  .parse(process.argv);
+
+if (sourceValue === undefined) {
+  program.outputHelp();
+  quitWithError('No source file specified.');
+}
+
+if (targetValue === undefined) {
+  program.outputHelp();
+  quitWithError('No target file specified.');
+}
+
+fs.readFile(sourceValue!, 'utf8', (error, data) => {
+  if (error) {
+    quitWithError(error);
+  }
+  const json = JSON.parse(data);
+  const json2sav = new Json2Sav(json);
+  const output = json2sav.transform();
+
+  fs.writeFile(targetValue!, output, 'binary', (error2) => {
+    if (error2) {
+      quitWithError(error2);
+    }
+    console.log('Converted ' + sourceValue + ' to ' + targetValue);
+  });
+});
---
+++ src/cli/sav2json.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+import * as fs from 'fs';
+import * as program from 'commander';
+
+import { Sav2Json } from '../sav2json';
+
+let sourceValue: string | undefined;
+let targetValue: string | undefined;
+
+function quitWithError(message: any) {
+  console.error(message);
+  process.exit(1);
+}
+
+program
+  .description('Converts Satisfactory save games (.sav) into a more readable format (.json)')
+  .arguments('<source> <target>')
+  .action((source, target) => {
+    sourceValue = source;
+    targetValue = target;
+  })
+  .parse(process.argv);
+
+if (sourceValue === undefined) {
+  program.outputHelp();
+  quitWithError('No source file specified.');
+}
+
+if (targetValue === undefined) {
+  program.outputHelp();
+  quitWithError('No target file specified.');
+}
+
+fs.readFile(sourceValue!, 'binary', (error, data) => {
+  if (error) {
+    quitWithError(error);
+  }
+  const sav2json = new Sav2Json(Buffer.from(data, 'binary'));
+  const output = JSON.stringify(sav2json.transform());
+
+  fs.writeFile(targetValue!, output, 'utf8', (error2) => {
+    if (error2) {
+      quitWithError(error2);
+    }
+    console.log('Converted ' + sourceValue + ' to ' + targetValue);
+  });
+});
